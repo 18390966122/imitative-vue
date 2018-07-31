4d8602032a5c3432ddc4f13be829aabd5ca01cbe
caoxiaoli <caoxiaoli@zlg.cn>
2018-07-31 14:48:07 +0800

    仿照vue的双向数据传递模型基础完成

--- cors/compile.js
+++ cors/compile.js
@@ -4,33 +4,77 @@ function Compile(el, vm) {
 }
 
 Compile.prototype = {
-    nodeToFragment (el, vm) {
+    nodeToFragment(el, vm) { // 返回代码片段，直接把代码片段重新渲染进html里面
         let child = null;
         let frag = document.createDocumentFragment()
-        while(child = el.firstChild) {
-            this.nodeToVlue(child, vm)
+        while (child = el.firstChild) {
+            this.nodeToVlue(child, vm) // 把循环元素node 把每个元素加进fragment里面
             frag.appendChild(child)
         }
         return frag
     },
-    nodeToVlue (node, vm) {
-        let reg = /\{\{(.*)\}\}/
+    nodeToVlue(node, vm) {
+        let reg = /\{\{(.*)\}\}/g
+        let array = this.circulatedata(vm.data) // 这里利用递归的方法，把多层data的值和键做了处理
         if (node.nodeType === 1) {
             let name = node.getAttribute('v-model')
-            if (name === 'testText') {
-                node.value = vm.data[name]
-                node.addEventListener('input', function(e) {
-                    vm.data[name] = e.target.value
-                })
-                new Watcher(vm, node, name, 'value')
-            }
+            let html = node.innerHTML
+            array.forEach((v, index) => {
+                if (name === v.key) { // 属性为v-model，并且元素是select input textarea
+                    if (node.nodeName === 'SELECT') {
+                        let child = node.childNodes
+                        Object.keys(child).forEach((value, index) => { // 遍历select的子节点 option  并设置选中的值
+                            if (child[index].nodeType === 1) {
+                                child[index].setAttribute('selected', false)
+                                if (child[index].value === v.value) {
+                                    child[index].setAttribute('selected', true)
+                                }
+                            }
+                        })
+                        node.addEventListener('change', function (e) {
+                            vm.data[v.key] = e.target.value
+                        })
+                    } else {
+                        node.addEventListener('input', function (e) {
+                            vm.data[v.key] = e.target.value
+                        })
+                    }
+                    node.value = v.value
+                    new Watcher(vm, node, v.key, 'value')
+                } else if (html !== '') { // 普通的双标签元素，内容是{{}}格式的
+                    let name = RegExp.$1
+                    node.nodeValue = array
+                    new Watcher(vm, node, v.key, 'innerHTML')
+                }
+            })
         }
-        if (node.nodeType === 3) {
+        if (node.nodeType === 3) { // 文本格式的{{}}数据
             if (reg.test(node.nodeValue)) {
                 let name = RegExp.$1
-                node.nodeValue = vm.data[name]
-                new Watcher(vm, node, name, 'nodeValue')
+                array.forEach((v, i) => {
+                    if (name === v.key) {
+                        node.nodeValue = v.value
+                        new Watcher(vm, node, name, 'nodeValue')
+                    }
+                })
             }
         }
+    },
+    circulatedata(data, arr, key) { // 这里利用递归方法，把data中的所有变量和值转换成  {key: data中数据对应在页面上书写形式的键, value：对应data的值}
+        let array = arr || []
+        for (let k in data) {
+            let obj = {}
+            obj.key = key || ''
+            obj.value = null
+            if (typeof data[k] === 'object') {
+                let objKey = key ? (key + '.' + k) : k 
+                this.circulatedata(data[k], array, objKey)
+            } else {
+                obj.key = obj.key === '' ? obj.key + k : obj.key + '.' + k
+                obj.value = data[k]
+                array.push(obj)
+            }
+        }
+        return array
     }
 }
--- cors/watcher.js
+++ cors/watcher.js
@@ -13,21 +13,58 @@ Dep.prototype = {
         })
     }
 }
-
-
+/**
+ * 给节点node添加订阅者管理
+ * @param {Object} vm 初始化 new Vue 的对象
+ * @param {Object} node 需要添加订阅者管理的node节点
+ * @param {Object} name 是页面上的值的变量，比如： testText ， one.two
+ * @param {Object} keys 获取node节点间的值类型  例如:input 是value;  div标签是innerHTML
+ */
 function Watcher(vm, node, name, keys) {
     Dep.target = this
     this.vm = vm
     this.node = node
     this.name = name
     this.keys = keys
+    // oldValue 这是记住data改变的上一个值，为了解决变量和字符串混合值的解析和同步改变，
+    // 比如：<div>这是测试额外字符的 {{options}}</div> 解析div里面的options变量
+    this.oldValue = null
     this.update()
     Dep.target = null
 }
 
 Watcher.prototype = {
     update () {
-        this.node[this.keys] = this.vm.data[this.name]
+        let reg = /\{\{(.*)\}\}/
+        let current = this.val || this.translateKeys(this.vm, this.name)
+        if(this.keys === 'innerHTML') {
+            if (reg.test(this.node[this.keys])) { // 判断是首次解析渲染还是渲染之后再改变这个变量
+                let name = RegExp.$1
+                this.node[this.keys] = this.node[this.keys].replace(`{{${name}}}`, this.translateKeys(this.vm, name))
+            } else {
+                this.node[this.keys] = this.node[this.keys].replace(this.oldValue, current)
+            }
+            this.oldValue = current
+        } else {
+            this.node[this.keys] = this.translateKeys(this.vm, this.name)
+        }
+    },
+    translateKeys (vm, keys) { // 把字符串的键值转换为值输出
+        let array = keys.split('.')
+        if (typeof vm.data[array[0]] === 'object') {
+            return this.toVlalue(vm.data[array[0]])
+        } else {
+            return vm.data[array[0]]
+        }
+    },
+    toVlalue (obj) { // 利用递归的方法，把多层对象的值遍历出来返回
+        for (let k in obj) {
+            if (typeof obj[k] === 'object') {
+                this.toVlalue(obj[k])
+            } else {
+                return obj[k]
+            }
+        }
     }
 }
 
